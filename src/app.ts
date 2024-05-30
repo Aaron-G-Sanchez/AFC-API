d5c88eb1a41e72cd176590da5fad892b42545abf
Aaron Sanchez <saarong12@gmail.com>
2024-05-29 23:05:11 -0500

    Added morgan for logging

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Express, Request, Response, NextFunction } from 'express'
+import morgan from 'morgan'
 
 export const app: Express = express()
 
+app.use(morgan('dev'))
+
 app.use('/hello', async (req: Request, res: Response, next: NextFunction) => {
   res.send({ msg: 'world' })
+  next()
 })
