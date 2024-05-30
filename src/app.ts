8923bcf65a5f440beb6f3bae37ecd11870ef21e3
Aaron Sanchez <saarong12@gmail.com>
2024-05-30 00:09:36 -0500

    Added dotenv package and added api token for sportmonks.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response, NextFunction } from 'express'
+import 'dotenv/config'
 import morgan from 'morgan'
+
+const API_TOKEN = process.env.API_TOKEN
 
 export const app: Express = express()
 
@@ -9,3 +12,9 @@ app.use('/hello', async (req: Request, res: Response, next: NextFunction) => {
   res.send({ msg: 'world' })
   next()
 })
+
+// TODO Add route to fetch next match.
+
+// TODO Add route to fetch remaining games.
+
+// TODO Add route to fetch all players.
