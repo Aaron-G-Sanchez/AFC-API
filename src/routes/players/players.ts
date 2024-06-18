982a98b591f3c9c16f027654bb64c903fbe822c2
Aaron Sanchez <saarong12@gmail.com>
2024-06-18 00:26:08 -0500

    Added fetch to get all the current players from external api

--- src/routes/players/players.ts
+++ src/routes/players/players.ts
@@ -1,11 +1,21 @@
 import express, {Express, Request, Response, NextFunction, Router} from 'express'
+import 'dotenv/config'
 
+const API_TOKEN = process.env.API_TOKEN
 const router: Router = express.Router()
 
 // TODO: Add functionality. 
 // Get a list of all the players.
 router.get('/', async(req: Request, res: Response, next: NextFunction) => {
-  res.send({msg: "Players endpoint hit!"})
+  const afcID: number = 254172
+  const baseRoute: string = `https://api.sportmonks.com/v3/football/teams/${afcID}?api_token=${API_TOKEN}&include=players`
+
+  // Fetch the data from the sportmonks api
+  const apiRes = await fetch(baseRoute)
+  const json = await apiRes.json()
+
+  // TODO: Need to loop through the json and isolate the players.
+  res.send(json)
 })
 
 export default router
